484a073fd86836a2827ca653c04c8a365131af5e
subhankar12-spec <subhankarmondal360@gmail.com>
2024-10-08 11:58:15 +0530

    Project SKeleton

---
+++ src/main/java/com/frozeneye/ecommerce/EcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.frozeneye.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/frozeneye/ecommerce/controllers/TestController.java
@@ -0,0 +1,15 @@
+package com.frozeneye.ecommerce.controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/hello")
+    public String login() {
+        return "Hello World man";
+    }
+
+}
